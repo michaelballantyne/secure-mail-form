482e830fe981eecdb290344ddc79eb9c1e80ffe4
Michael Ballantyne <michael.ballantyne@gmail.com>
2013-06-13 00:19:56 -0600

    improve look, add sent and error pages

--- mail.py
+++ mail.py
@@ -1,22 +1,24 @@
 import cgi, smtplib
 from email.mime.text import MIMEText
 def app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    
     if environ['REQUEST_METHOD'] == 'POST':
-        form = cgi.FieldStorage(fp=environ['wsgi.input'], 
-                            environ=environ)
-        body = form['encrypted'].value
-        addr = "michael.ballantyne@gmail.com"
-        msg = MIMEText(body)
-        msg['Subject'] = "Secure message form"
-        msg['From'] = addr
-        msg['To'] = addr
+        try:
+            form = cgi.FieldStorage(fp=environ['wsgi.input'], 
+                                environ=environ)
+            body = form['encrypted'].value
+            addr = "michael.ballantyne@gmail.com"
+            msg = MIMEText(body)
+            msg['Subject'] = "Secure message form"
+            msg['From'] = addr
+            msg['To'] = addr
 
-        s = smtplib.SMTP('localhost')
-        s.sendmail(addr, [addr], msg.as_string())
-        s.quit()
+            s = smtplib.SMTP('localhost')
+            s.sendmail(addr, [addr], msg.as_string())
+            s.quit()
 
-        return ['Thanks! Your message has been sent.\n']
+            start_response('301 Redirect', [('Location', 'sent.html'),])
+            return []
+
+        except:
+            start_response('301 Redirect', [('Location', 'failed.html'),])
+            return []
