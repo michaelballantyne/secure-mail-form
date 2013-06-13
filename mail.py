4a0bdb06d6e4c062a8ed869181ab4073526b2f34
Michael Ballantyne <michael.ballantyne@gmail.com>
2013-06-12 20:26:00 -0600

    initial

---
+++ mail.py
@@ -0,0 +1,21 @@
+import cgi, smtplib
+from email.mime.text import MIMEText
+def app(environ, start_response):
+    if environ['REQUEST_METHOD'] == 'POST':
+        form = cgi.FieldStorage(fp=environ['wsgi.input'], 
+                            environ=environ)
+        body = form['encrypted'].value
+        addr = "michael.ballantyne@gmail.com"
+        msg = MIMEText(body)
+        msg['Subject'] = "Secure message form"
+        msg['From'] = addr
+        msg['To'] = addr
+
+        s = smtplib.SMTP('localhost')
+        s.sendmail(addr, [addr], msg.as_string())
+        s.quit()
+
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return ['Hello world!\n']
