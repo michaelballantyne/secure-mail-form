c47c03650f4a165cd7f7d852711ace21190f47dc
Michael Ballantyne <michael.ballantyne@gmail.com>
2013-06-13 01:31:12 -0600

    change email back

--- mail.py
+++ mail.py
@@ -1,4 +1,4 @@
-import cgi, smtplib, sys
+import cgi, smtplib, sys, datetime
 from email.mime.text import MIMEText
 def application(environ, start_response):
     if environ['REQUEST_METHOD'] == 'POST':
@@ -6,10 +6,10 @@ def application(environ, start_response):
             form = cgi.FieldStorage(fp=environ['wsgi.input'], 
                                 environ=environ)
             body = form['encrypted'].value
-            fromaddr = "securemessage@mballantyne.net"
+            fromaddr = "michael.ballantyne@gmail.com"
             toaddr = "michael.ballantyne@gmail.com"
             msg = MIMEText(body)
-            msg['Subject'] = "Secure message"
+            msg['Subject'] = "Secure message" + datetime.datetime.
             msg['From'] = fromaddr
             msg['To'] = toaddr
 
